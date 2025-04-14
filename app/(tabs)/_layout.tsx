ea0ff7b3d204ac1d5435f7645129958d73bf19cd
yangliudev <ylyangliu1@gmail.com>
2025-04-14 16:05:27 -0700

    -

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,9 +39,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: "Explore",
+          title: "Image Gen",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <IconSymbol size={28} name="image.gen" color={color} />
           ),
         }}
       />
